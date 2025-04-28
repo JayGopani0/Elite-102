0e60f96ce58fb2f00a7e8ec5920e14d986059995
Jay Gopani <jaygopani@Jays-MacBook-Air.local>
2025-04-27 23:15:45 -0400

    start of work

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+import mysql.connector
+
+connection = mysql.connector.connect(user = 'root', database = 'example_schema', password = 'SQL.12454933')
+
+cursor = connection.cursor()
+
+testQuery = ("SELECT * FROM example_table")
+
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+
+
+connection.commit()
+cursor.close()
+connection.close()
