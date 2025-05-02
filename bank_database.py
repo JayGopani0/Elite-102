91946eff3d6c3ffb00adfcde79766f470779c1f1
Jay Gopani <jaygopani@Jays-MacBook-Air.local>
2025-05-02 00:43:12 -0400

    Finished Functionality

--- bank_database.py
+++ bank_database.py
@@ -6,19 +6,61 @@ cursor = connection.cursor()
 
 
 def view_all_data(): 
-    testQuery = ("SELECT * FROM bank_database")
-    cursor.execute(testQuery)
+    query = ("SELECT * FROM bank_database") # Selects all data from database 
+    cursor.execute(query)
     for item in cursor:
         print(item)
 
-
+# Resets the database in order to delete previous data and reset ID Auto increment
 def delete_all_data():
-    testQuery = ("TRUNCATE TABLE bank_database")
-    cursor.execute(testQuery)
+    query = ("TRUNCATE TABLE bank_database") 
+    cursor.execute(query)
+    connection.commit()
 
-
+# Allows the user to create an account with a name, pin and balance.
 def insert_account(name, account_number, pin, balance):
-    testQuery = ("INSERT INTO bank_database (name, account_number, pin, balance) VALUES (%s, %s, %s, %s)")
+    query = ("INSERT INTO bank_database (name, account_number, pin, balance) VALUES (%s, %s, %s, %s)")
     values = (name, account_number, pin, balance)
-    cursor.execute(testQuery, values)
+    cursor.execute(query, values)
+    connection.commit()
 
+# Allows the user to delete a specific account based on pin and account number given 
+def delete_account(account_number, pin):
+    query = "DELETE FROM bank_database WHERE account_number = %s AND pin = %s"
+    values = (account_number, pin)
+    cursor.execute(query, values)
+    connection.commit()
+
+# User can edit the name of account with proper account number and with the new name
+def edit_account_name(account_number, new_name):
+    cursor.execute("UPDATE bank_database SET name = %s WHERE account_number = %s", (new_name, account_number))
+    connection.commit()
+
+# User can edit the account number of account with the previous account number and with the number
+def edit_account_number(account_number, new_number):
+    cursor.execute("UPDATE bank_database SET account_number = %s WHERE account_number = %s", (new_number, account_number))
+    connection.commit()
+
+# User can edit the pin of account with proper account number and with the new pin number
+def edit_account_pin(account_number, new_pin):
+    cursor.execute("UPDATE bank_database SET pin = %s WHERE account_number = %s", (new_pin, account_number))
+    connection.commit()
+
+# Allows user to check details on the specific account given the pin and account number
+def check_balance(account, pin):
+    cursor.execute("SELECT balance FROM bank_database WHERE account_number = %s AND pin = %s", (account, pin))
+    data = cursor.fetchone()
+    print("Balance:", data[0])
+
+
+# Allows user to add money into an existing account given the pin and account number
+def deposit(account, pin, amount):
+    cursor.execute("UPDATE bank_database SET balance = balance + %s WHERE account_number = %s AND pin = %s", (amount, account, pin))
+    connection.commit()
+    print("Money added successfully.")
+
+# Allows user to withdraw money from an existing account given the pin and account number
+def withdraw(account, pin, amount):
+    cursor.execute("SELECT balance FROM bank_database WHERE account_number = %s AND pin = %s", (account, pin))
+    data = cursor.fetchone()
+    print("Money was taken out successfully.")
--- main.py
+++ main.py
@@ -2,14 +2,72 @@ import bank_database
 import random
 
 
+# Creates an account using bank_database functions
 def create_account(): 
-    number = random.randint(10000,99999)
+    number = random.randint(10000,99999) #randomly generates an account number 
     name = input("Enter name: ")
     pin = int(input("Enter a 4-digit pin: "))
     balance = int(input("Enter the amount of money you are initially depositing: "))
-    bank_database.insert_account(name, number, pin, balance)
-    
+    bank_database.insert_account(name, number, pin, balance) 
+    print("Account successfully added.")
 
-bank_database.delete_all_data()
-create_account()
-bank_database.view_all_data()
+# Deletes an account using bank_database functions
+def delete_account():
+    account_number = input("Enter your account number: ")
+    pin = input("Enter your PIN: ")
+    if bank_database.delete_account(account_number, pin):
+        print("Account successfully deleted.")
+    else:
+        print("Account not found or incorrect PIN.")
+
+def edit_account():
+    print("What would you like to edit? Name, account number or pin? ")
+    edit = input("N/A/P: ")
+    number = int(input("Enter account number: "))
+    if edit == "N":
+        new_name = input("Enter new name: ")
+        bank_database.edit_account_name(number, new_name)
+    elif edit == "A":
+        new_number = int(input("Enter new number: "))
+        bank_database.edit_account_number(number, new_number)
+    else:
+        new_pin = int(input("Enter new pin: "))
+        bank_database.edit_account_pin(number, new_pin)
+
+def manage_transaction():
+    print("Do you want to withdraw(W), deposit(D) or check balance(B)? ")
+    decision = input("W/D/B: ")
+    account_number = int(input("Enter account number: "))
+    pin = input("Enter your PIN: ")
+    if decision == "W":
+        amount = int(input("Enter the amount: "))
+        bank_database.withdraw(account_number, pin, amount)
+    elif decision == "D": 
+        amount = int(input("Enter the amount: "))
+        bank_database.deposit(account_number, pin, amount)
+    else:
+        bank_database.check_balance(account_number, pin)
+
+
+if __name__ == "__main__":
+    stay = True
+    while stay:
+        print("Hello and welcome to the bank!")
+        print("What would you like to do today? Manage an account(A) or manage transactions(T)?: ")
+        decision = input("A/T? ")
+        if decision == "A":
+            print("Do you want to create(C), delete(D) or edit(E) an account?")
+            manage = input("C/D/E: ")
+            if manage == "C":
+                create_account()
+            if manage == "D":
+                delete_account()
+            if manage == "E": 
+                edit_account()
+        elif decision == "T":
+            manage_transaction()
+
+        stay = input("Would you like to stay or exit? (E to exit): ")
+        if stay == "E":
+            break
+   
